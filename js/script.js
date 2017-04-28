07143687939a2b053f40b81e5d1425a84794d479
Harryki <zgp7777@gmail.com>
2017-04-27 22:08:39 -0700

    resume web page
    null

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Shorthand for $( document ).ready()
+$(function() {
+	console.log( "ready!" );
+	
+	var currentYear = (new Date).getFullYear();
+	$('#year').text(currentYear);
+});
+
+function myFunction(){
+	$("#intro_div").slideToggle();
+	$('#intro_icon').toggleClass("fa-minus");
+	$('#intro_icon').toggleClass("fa-plus");
+
+}
+function myFunction2(){
+	$("#skills_div").slideToggle();
+	$('#skills_icon').toggleClass("fa-minus");
+	$('#skills_icon').toggleClass("fa-plus");
+
+}function myFunction3(){
+	$("#contact_div").slideToggle();
+	$('#contact_icon').toggleClass("fa-minus");
+	$('#contact_icon').toggleClass("fa-plus");
+
+}
+
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+})
+
+$('#myAlert').on('closed.bs.alert', function () {
+  // do something…
+  console.log("ddd");  
+})
