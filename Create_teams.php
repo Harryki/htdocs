662fa918122cc9cff444deba5ba6180163a55588
Harryki <zgp7777@gmail.com>
2017-06-23 17:25:33 -0700

    fdsa
    fdas

--- Create_teams.php
+++ Create_teams.php
@@ -8,15 +8,41 @@
 
 // Let's make sure the file exists and is writable first.
 	$handle = fopen($filename,'w+'); 
-	fwrite($handle, $somecontent);
-	fclose($handle);
+	if (is_writable($filename)) {
+
+    // In our example we're opening $filename in append mode.
+    // The file pointer is at the bottom of the file hence
+    // that's where $somecontent will go when we fwrite() it.
+		if (!$handle = fopen($filename, 'a')) {
+			echo "Cannot open file ($filename)";
+			exit;
+		}
+
+    // Write $somecontent to our opened file.
+		if (fwrite($handle, $somecontent) === FALSE) {
+			echo "Cannot write to file ($filename)";
+			exit;
+		}
+
+		echo "Success, wrote to file ($filename)";
+
+		fclose($handle);
+
+	} else {
+		echo "The file $filename is not writable";
+	}
 
 	// this works
 	$key_value = "?name=" . $team_name;
 
 	echo $_POST["hidden_userid"];
+
+	//add some database work related to team table
+	
 	?>
 	
-	<script type="text/javascript">location.href = '<?php echo $file_name . $key_value ?>';</script>
+	<script type="text/javascript">
+		location.href = '<?php echo $filename . $key_value ?>';
+	</script>
 </body>	
 </html>
